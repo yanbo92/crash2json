178c62e13d43af2840993cdb759ec62b241aa78f
liyanbo <liyanbo@insta360.com>
2021-11-22 00:08:24 +0800

    update main and setup

--- crash2json/__main__.py
+++ crash2json/__main__.py
@@ -6,7 +6,7 @@ import argparse
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(__file__))
 from crash2json._crash2json import Crash2Json
 
 
