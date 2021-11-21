ab6bd692fb03c29b498bc98ab12b874043915d35
liyanbo <liyanbo@insta360.com>
2021-11-21 22:29:29 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/21 10:26 下午
+# @Author  : yanbo92
+# @File    : setup.py
+
+from distutils.core import setup
+
+setup(name='crash2json',
+      version='1.0',
+      description='Parse appple crash report to json file',
+      author='yanbo92',
+      author_email='yanbo92@139.com',
+      url='https://github.com/yanbo92/crash2json',
+      packages=['crash2json'],
+     )
