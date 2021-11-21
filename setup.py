19a8769e39d591bbd15ad032196bb1e344a9ba37
liyanbo <liyanbo@insta360.com>
2021-11-21 23:04:06 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@
 from distutils.core import setup
 
 setup(name='crash2json',
-      version='1.0',
+      version='1.1',
       description='Parse appple crash report to json file',
       author='yanbo92',
       author_email='yanbo92@139.com',
       url='https://github.com/yanbo92/crash2json',
       packages=['crash2json'],
-     )
+      entry_points={
+          'console_scripts': [
+              'crash2json.__main__:main'
+          ]
+      },
+      )
